dafd043de1da318296f124987e04b80f859411f8
chomok16 <a01023660652@gmail.com>
2024-06-18 15:29:00 +0900

    Update chat.py

--- pages/chat.py
+++ pages/chat.py
@@ -1,7 +1,7 @@
 from openai import OpenAI
 import streamlit as st
 
-userkey=st.input()
+userkey=st.text_input
 client = OpenAI(api_key=userkey)
 #assistant = client.beta.assistants.create(
 #    instructions="당신의 이름은 백경AI입니다. 친근한 말투로 대답해주세요. 챗봇으로서 성실하게 대답해주세요.",
