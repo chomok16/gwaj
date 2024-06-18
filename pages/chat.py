903f2bc58e374a678d4ff5ce30c89ba10449db1f
chomok16 <a01023660652@gmail.com>
2024-06-18 15:22:25 +0900

    Update chat.py

--- pages/chat.py
+++ pages/chat.py
@@ -5,4 +5,4 @@ client = OpenAI(api_key='sk-proj-asdfasdfdasfafsdfasdasfd')
 #    instructions="당신의 이름은 백경AI입니다. 친근한 말투로 대답해주세요. 챗봇으로서 성실하게 대답해주세요.",
 #    model="gpt-4o",
 #)
-st.markdown(client.id)
+st.markdown(client)
