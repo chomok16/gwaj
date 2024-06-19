2d3cad3a38eaaa49a596afcf1185063933e9571a
chomok16 <a01023660652@gmail.com>
2024-06-19 15:46:01 +0900

    Update testuu.py

--- pages/testuu.py
+++ pages/testuu.py
@@ -70,7 +70,7 @@ if prompt := st.chat_input("메시지를 입력하세요."):
         thread_messages = client.beta.threads.messages.list(thread.id, run_id=run.id)
         answer = thread_messages.data[0].content[0].text.value # assistant의 응답에서 text를 추출
         message(answer)
-        st.session_state.messages.append({"role": "assistant", "content": answer})
+        st.session_state.messages.append({"content": answer, "role": False})
 if st.button("대화 내역 지우기"):
     if st.session_state.thread_id:
         if st.session_state.client:
