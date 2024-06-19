fb157c4a137be68f29a6368bbd13e2d4280f1227
chomok16 <a01023660652@gmail.com>
2024-06-20 02:41:59 +0900

    Update 백경AI.py

--- pages/백경AI.py
+++ pages/백경AI.py
@@ -120,13 +120,13 @@ if prompt := st.chat_input("메시지를 입력하세요."):
         ]
         for bldg_data_component in bldg_data:
             if bldg_data["name"] in prompt:
-                img_path="https://github.com/chomok16/gwaj/blob/main/maps/%EB%8C%80%ED%95%99%EB%B3%B8%EB%B6%80.png?raw=true"
+                img_path=bldg
                 img_data=f'<img width="100%" height="100%" src="{img_path}"/>'
                 message(img_data, avatar_style = 'no-avatar', allow_html = True)
                 st.session_state.messages.append({"content": img_data, "is_user": False, "html": True})
-            elif bld_data["name"] not in prompt:
-                if bld_data["code"] in prompt:
-                    img_path=bld_data["img_url"]
+            elif bldg_data["name"] not in prompt:
+                if bldg_data["code"] in prompt:
+                    img_path=bldg_data["img_url"]
                     img_data=f'<img width="100%" height="100%" src="{img_path}"/>'
                     message(img_data, avatar_style = 'no-avatar', allow_html = True)
                     st.session_state.messages.append({"content": img_data, "is_user": False, "html": True})
