4d0fcdbb7e5e4a1e1430849d07acf2b6ebb99592
chomok16 <a01023660652@gmail.com>
2024-06-19 20:06:44 +0900

    Update testuuuuuuu.py

--- pages/testuuuuuuu.py
+++ pages/testuuuuuuu.py
@@ -3,4 +3,7 @@ from streamlit_chat import message
 from streamlit.components.v1 import html
 
 img_path="https://www.groundzeroweb.com/wp-content/uploads/2017/05/Funny-Cat-Memes-11.jpg"
-message(f'<img width="100%" height="200" src="{img_path}"/>')
+message(
+  f'<img width="100%" height="200" src="{img_path}"/>'
+  allow_html = True
+       )
