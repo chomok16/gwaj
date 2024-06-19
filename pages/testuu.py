71db2911957387d81ba8609b1299d13f30b63d31
chomok16 <a01023660652@gmail.com>
2024-06-19 15:50:43 +0900

    Update testuu.py

--- pages/testuu.py
+++ pages/testuu.py
@@ -16,35 +16,34 @@ with st.sidebar:
     if user_api_key := st.text_input("OpenAI API키를 입력해주세요.", key = "openai_api_key", type="password"):
         if 'user_api_key' not in st.session_state:
             st.session_state.key = user_api_key
-    
-if st.button('Assistant 새롭게 생성하기'):
-    if st.session_state.key:
-        client = OpenAI(api_key=user_api_key)
-        vector_store=client.beta.vector_stores.create(name="TotalFile")
-        file_paths = ["메뉴와가격.pdf"]
-        file_streams = [open(path, "rb") for path in file_paths]
-        file_batch = client.beta.vector_stores.file_batches.upload_and_poll(
-            vector_store_id = vector_store.id,
-            files = file_streams
-        )
-        assistant = client.beta.assistants.create(
-            instructions="""
-            당신의 이름은 '백경이봇'입니다. 존댓말이 아닌 반말로 대답해주세요.
-            욕설은 사용하지 말아주세요. 챗봇으로서 성실하게 대답해주세요. 업로드된 파일을 바탕으로 대답해주세요.
-            vector_store에 업로드된 파일에 대해서는 언급하지 마세요.
-            """,
-            model="gpt-4o",
-            tools=[{"type": "file_search"}],
-            tool_resources={"file_search":{"vector_store_ids": [vector_store.id]}},
-        )
-        if 'client' not in st.session_state: # client를 session_state로 저장
-            st.session_state.client = client
-        if 'assistant' not in st.session_state: # assistant를 session_state로 저장
-            st.session_state.assistant = assistant
-        messages = []
-        st.session_state.messages = messages # 대화 내역을 session_state에 저장
-        message("안녕, 부경대 친구들, 학교생활을 도와주는 백경이야!", is_user=False)
-        st.session_state.messages.append({"content": "안녕, 부경대 친구들! 학교생활을 도와주는 '백경이봇'이야.", "role": False})
+    if st.button('Assistant 새롭게 생성하기'):
+        if st.session_state.key:
+            client = OpenAI(api_key=user_api_key)
+            vector_store=client.beta.vector_stores.create(name="TotalFile")
+            file_paths = ["메뉴와가격.pdf"]
+            file_streams = [open(path, "rb") for path in file_paths]
+            file_batch = client.beta.vector_stores.file_batches.upload_and_poll(
+                vector_store_id = vector_store.id,
+                files = file_streams
+            )
+            assistant = client.beta.assistants.create(
+                instructions="""
+                당신의 이름은 '백경이봇'입니다. 존댓말이 아닌 반말로 대답해주세요.
+                욕설은 사용하지 말아주세요. 챗봇으로서 성실하게 대답해주세요. 업로드된 파일을 바탕으로 대답해주세요.
+                vector_store에 업로드된 파일에 대해서는 언급하지 마세요.
+                """,
+                model="gpt-4o",
+                tools=[{"type": "file_search"}],
+                tool_resources={"file_search":{"vector_store_ids": [vector_store.id]}},
+            )
+            if 'client' not in st.session_state: # client를 session_state로 저장
+                st.session_state.client = client
+            if 'assistant' not in st.session_state: # assistant를 session_state로 저장
+                st.session_state.assistant = assistant
+            messages = []
+            st.session_state.messages = messages # 대화 내역을 session_state에 저장
+            message("안녕, 부경대 친구들, 학교생활을 도와주는 백경이야!", is_user=False)
+            st.session_state.messages.append({"content": "안녕, 부경대 친구들! 학교생활을 도와주는 '백경이봇'이야.", "role": False})
 
 
 if prompt := st.chat_input("메시지를 입력하세요."):
