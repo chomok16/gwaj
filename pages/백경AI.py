060e82e9659b312ba85f586a91827ab9d4ce487f
chomok16 <a01023660652@gmail.com>
2024-06-20 01:44:59 +0900

    Update 백경AI.py

--- pages/백경AI.py
+++ pages/백경AI.py
@@ -83,7 +83,7 @@ if prompt := st.chat_input("메시지를 입력하세요."):
         st.session_state.messages.append({"content": answer, "is_user": False, "html": False})
         if "대학본부" in prompt:
             img_path="https://github.com/chomok16/gwaj/blob/main/maps/%EB%8C%80%ED%95%99%EB%B3%B8%EB%B6%80.png?raw=true"
-            img_data=f'<img width="100%" height="200" src="{img_path}"/>'
+            img_data=f'<img width="100%" height="100%" src="{img_path}"/>'
             message(img_data, avatar_style = 'no-avatar', allow_html = True)
             st.session_state.messages.append({"content": img_data, "is_user": False, "html": True})
 if st.button("대화 내역 지우기"):
