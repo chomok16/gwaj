a818f8a16fa96b55d3a5958142d78b407d19a601
chomok16 <a01023660652@gmail.com>
2024-06-19 20:05:31 +0900

    Update testuuuuuuu.py

--- pages/testuuuuuuu.py
+++ pages/testuuuuuuu.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from streamlit_chat import message
+from streamlit.components.v1 import html
 
-img = open("백경이.png", 'r')
-message("hi")
+img_path="https://www.groundzeroweb.com/wp-content/uploads/2017/05/Funny-Cat-Memes-11.jpg"
+message(f'<img width="100%" height="200" src="{img_path}"/>')
